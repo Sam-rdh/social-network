c0078706874915a189c91d1b72e99fd2116cc7d3
Sami K <macbook@MacBook-Air-de-Sami.local>
2025-08-13 17:33:23 +0200

    Version Final

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/core/services/ui/ui.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private uiService : UiService) { }
 
   ngOnInit(): void {
+    this.uiService.closeSidebar();
   }
 
 }
